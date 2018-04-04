3297b7216732b9c1c2d10814330bacdcd633e6dc
Eric Budd <ericbudd@gmail.com>
2018-04-03 22:24:16 -0600

    user auth setup

--- routes/users.js
+++ routes/users.js
@@ -7,4 +7,23 @@ const router = express.Router();
 
 // YOUR CODE HERE
 
+// CREATE ONE record for this table
+router.post('/', (req, res, next) => {
+  knex('users')
+    .insert({
+      "first_name": req.body.title,
+      "last_name": req.body.author,
+      "email": req.body.genre,
+      "password": req.body.description,
+      "cover_url": req.body.coverUrl
+    })
+    .returning('*')
+    .then((data) => {
+      res.json(humps.camelizeKeys(data[0]))
+    })
+    .catch((err) => {
+      next(err)
+    })
+})
+
 module.exports = router;
