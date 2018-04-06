350f1e34fc4158edd33438eb07f3bb7a2bbb50dc
Eric Budd <ericbudd@gmail.com>
2018-04-06 14:31:35 -0600

    got the password hashed and all that

--- routes/users.js
+++ routes/users.js
@@ -3,44 +3,48 @@
 const express = require('express');
 const knex = require('../knex')
 const humps = require('humps')
-// eslint-disable-next-line new-cap
+const bcrypt = require('bcrypt');
+
 const router = express.Router();
 
 // YOUR CODE HERE
 
-// router.get('/', (req, res, next) => {
-//   knex('books')
-//     .orderBy('title')
-//     .then((rows) => {
-//       res.json(humps.camelizeKeys(rows))
-//     })
-//     .catch((err) => {
-//       next(err)
-//     })
-// })
+
 
 // CREATE ONE record for this table
 router.post('/', (req, res, next) => {
   // res.send(200)
-  knex('users')
-    .insert({
-      "first_name": req.body.firstName,
-      "last_name": req.body.lastName,
-      "email": req.body.email,
-      "hashed_password": req.body.password,
-    })
-    .returning([
-      "id",
-      "first_name",
-      "last_name",
-      "email"
-    ])
-    .then((data) => {
-      res.json(humps.camelizeKeys(data[0]))
-    })
-    .catch((err) => {
-      next(err)
-    })
+
+  //console.log(req.body.password);
+
+  bcrypt.hash(req.body.password, 12).then(function(hash) {
+    // Store hash in your password DB.
+    console.log(hash);
+    insertIntoDB(hash)
+  });
+
+
+  function insertIntoDB(hash) {
+    knex('users')
+      .insert({
+        "first_name": req.body.firstName,
+        "last_name": req.body.lastName,
+        "email": req.body.email,
+        "hashed_password": hash
+      })
+      .returning([
+        "id",
+        "first_name",
+        "last_name",
+        "email"
+      ])
+      .then((data) => {
+        res.json(humps.camelizeKeys(data[0]))
+      })
+      .catch((err) => {
+        next(err)
+      })
+  }
 })
 
 module.exports = router;
