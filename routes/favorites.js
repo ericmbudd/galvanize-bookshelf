780b9e16a0b54ec317354aaa847ffd3f17369e47
Eric Budd <ericbudd@gmail.com>
2018-04-24 10:29:06 -0600

    handle get unauthorized

--- routes/favorites.js
+++ routes/favorites.js
@@ -11,9 +11,7 @@ const router = express.Router();
 
 //console.log("favorites");
 
-const getFavorites = (req, res, next) => {
-  console.log("getFavorites");
-
+const checkAuth = (req, res, next) => {
   console.log(req.cookies["/token"]);
   if (!req.cookies["/token"]) {
     res.header("Content-Type", "text/plain")
@@ -22,32 +20,42 @@ const getFavorites = (req, res, next) => {
     res.status(401).send(
       "Unauthorized"
     );
-    //return next(boom.create(401, 'Unauthorized'));
-
   }
-
-
-  //const events_id = req.body.
-  //const users_id = jwt.verify(req.cookies.token, process.env.JWT_KEY).id
-  //res.locals.registered = false;
-
-  knex('favorites')
-    //  .where({
-    //      events_id,
-    //      users_id
-    //    })
-    //.join('events', 'events_users.events_id', 'events.id')
-    .then(favorites => {
-      console.log(favorites)
-      res.status(200).send(favorites)
-    })
-    .catch(err => {
-      next(err)
-    })
 }
 
-router.get('/', getFavorites)
-router.get('/:id', getFavorites)
+
+const getFavorites = (req, res, next) => {
+  console.log("getFavorites");
+
+
+  //return next(boom.create(401, 'Unauthorized'));
+
+}
+
+
+//const events_id = req.body.
+//const users_id = jwt.verify(req.cookies.token, process.env.JWT_KEY).id
+//res.locals.registered = false;
+
+knex('favorites')
+  //  .where({
+  //      events_id,
+  //      users_id
+  //    })
+  //.join('events', 'events_users.events_id', 'events.id')
+  .then(favorites => {
+    console.log(favorites)
+    res.status(200).send(favorites)
+  })
+  .catch(err => {
+    next(err)
+  })
+}
+
+router.get('/', checkAuth, getFavorites)
+router.get('/:id', checkAuth, getFavorites)
+router.post('/', checkAuth)
+router.delete('/', checkAuth)
 
 
 module.exports = router
